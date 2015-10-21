c22e1fce7de2d72915f52de4d5989b21e66837fb
ao <andrew@hsmoore.com>
2015-10-21 16:44:40 +0100

    Delivery Address additions

---
+++ worldpay-lib-dotnet-3-5/Worldpay.Sdk/Models/DeliveryAddress.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Runtime.Serialization;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+using Worldpay.Sdk.Enums;
+
+namespace Worldpay.Sdk.Models
+{
+    [Serializable, DataContract]
+    public class DeliveryAddress : Address
+    {
+        [DataMember]
+        public string firstName { get; set; }
+
+        [DataMember]
+        public string lastName { get; set; }
+
+    }
+}
